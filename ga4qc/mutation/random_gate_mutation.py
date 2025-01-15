638382c4ea6da42b788219729a399ad2ed2b6c68
westoun <christoph.werner.stein@gmail.com>
2025-01-15 13:46:55 +0100

    Add gate prob parameter

--- ga4qc/mutation/random_gate_mutation.py
+++ ga4qc/mutation/random_gate_mutation.py
@@ -7,13 +7,15 @@ from ga4qc.circuit import Circuit, GateSet
 class RandomGateMutation(IMutation):
 
     prob: float
+    gate_prob: float
     gate_set: GateSet
 
-    def __init__(self, gate_set: GateSet, prob: float):
+    def __init__(self, gate_set: GateSet, circ_prob: float, gate_prob: float):
         self.gate_set = gate_set
-        self.prob = prob
+        self.prob = circ_prob
+        self.gate_prob = gate_prob
 
     def mutate(self, circuit: Circuit) -> None:
         for i, gate in enumerate(circuit.gates):
-            if random.random() < self.prob:
+            if random.random() < self.gate_prob:
                 circuit.gates[i] = self.gate_set.random_gate()

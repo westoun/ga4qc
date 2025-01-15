6452927c076b7ed75eda4b9bb494b335c6493583
westoun <christoph.werner.stein@gmail.com>
2025-01-15 17:49:19 +0100

    Add callback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from typing import List
 
 from ga4qc.ga import GA
 from ga4qc.seeder import RandomSeeder
@@ -12,8 +13,21 @@ from ga4qc.selection import ISelection, TournamentSelection
 from ga4qc.circuit import GateSet, Circuit
 from ga4qc.circuit.gates import Identity, CX, S, T, H
 
+
+class PrintBestFitness(ICallback):
+    _generation = 0
+
+    def __call__(self, circuits: List[Circuit]) -> None:
+        self._generation += 1
+
+        fitness_scores = [circuit.fitness_values[0] for circuit in circuits]
+        best_fitness = min(fitness_scores)
+
+        print(f"Best fitness at gen {self._generation}: {best_fitness}")
+
+
 if __name__ == "__main__":
-    gate_set = GateSet([Identity, CX, S, T, H], qubit_num=3)
+    gate_set = GateSet([CX, S, T, H], qubit_num=4)
 
     fitness = None
 
@@ -23,9 +37,15 @@ if __name__ == "__main__":
         crossovers=[OnePointCrossover()],
         processors=[
             QuasimSimulator(),
-            JensenShannonFitness(target_dists=[np.array([0.5, 0, 0, 0, 0, 0, 0, 0.5])]),
+            JensenShannonFitness(
+                target_dists=[
+                    np.array([0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5])
+                ]
+            ),
         ],
         selection=TournamentSelection(tourn_size=2),
     )
 
-    ga.run(population_size=100, gate_count=20, generations=10)
+    ga.on_after_generation(PrintBestFitness())
+
+    population = ga.run(population_size=100, gate_count=5, generations=10)

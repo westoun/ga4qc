41eea0a6cf381c7d17eac3ffd114d268b51d4543
westoun <christoph.werner.stein@gmail.com>
2025-01-16 10:05:17 +0100

    Implement elitism

--- ga4qc/ga/ga.py
+++ ga4qc/ga/ga.py
@@ -5,7 +5,7 @@ from ga4qc.mutation import IMutation
 from ga4qc.crossover import ICrossover
 from ga4qc.circuit import Circuit, GateSet
 from ga4qc.processors import ICircuitProcessor
-from ga4qc.selection import ISelection
+from ga4qc.selection import ISelection, BestSelection
 from ga4qc.seeder import ISeeder
 from ga4qc.callback import ICallback
 
@@ -17,6 +17,8 @@ class GA:
     crossovers: List[ICrossover]
     processors: List[ICircuitProcessor]
     selection: ISelection
+
+    best_selection: ISelection
 
     after_generation_callbacks: List[ICallback]
 
@@ -36,17 +38,27 @@ class GA:
         self.processors = processors
         self.selection = selection
 
+        self.best_selection = BestSelection()
         self.after_generation_callbacks = []
 
     def on_after_generation(self, callback: ICallback) -> None:
         self.after_generation_callbacks.append(callback)
 
-    def run(self, population_size: int, gate_count: int, generations: int):
+    def run(
+        self,
+        population_size: int,
+        gate_count: int,
+        generations: int,
+        elitism_count: int = 0,
+    ):
         population = self.seeder.seed(population_size, gate_count)
 
         for generation in range(1, generations + 1):
 
-            # TODO: Perform Elitism
+            if generation == 1:
+                elite = []
+            else:
+                elite = self.best_selection.select(population, k=elitism_count)
 
             offspring = [circuit.copy() for circuit in population]
 
@@ -67,7 +79,9 @@ class GA:
             for processor in self.processors:
                 processor.process(offspring)
 
-            population = self.selection.select(offspring, population_size)
+            population = elite + self.selection.select(
+                offspring, population_size - elitism_count
+            )
 
             for callback in self.after_generation_callbacks:
                 callback(population, generation)

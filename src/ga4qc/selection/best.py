57db2c5fc7a08011dd9d73b6db223c5d09c574fd
Christoph Stein <christophstein@Christophs-MacBook-Pro.local>
2025-03-19 12:58:48 +0100

    Add init parameter for objective index

--- src/ga4qc/selection/best.py
+++ src/ga4qc/selection/best.py
@@ -6,10 +6,14 @@ from .interface import ISelection
 
 
 class BestSelection(ISelection):
+    objective_i: int
+
+    def __init__(self, objective_i: int = 0):
+        self.objective_i = objective_i
 
     def select(self, circuits: List[Circuit], k: int, generation: int) -> List[Circuit]:
         sorted_circuits = sorted(
-            circuits, key=lambda circuit: circuit.fitness_values[0]
+            circuits, key=lambda circuit: circuit.fitness_values[self.objective_i]
         )
 
         selection = [circuit.copy() for circuit in sorted_circuits[:k]]
--- src/ga4qc/selection/tournament.py
+++ src/ga4qc/selection/tournament.py
@@ -7,16 +7,19 @@ from .interface import ISelection
 
 class TournamentSelection(ISelection):
     tourn_size: int
+    objective_i: int
 
-    def __init__(self, tourn_size: int = 2):
+    def __init__(self, tourn_size: int = 2, objective_i: int = 0):
         self.tourn_size = tourn_size
+        self.objective_i = objective_i
 
     def select(self, circuits: List[Circuit], k: int, generation: int) -> List[Circuit]:
         selection = []
 
         for _ in range(k):
             contestors = [choice(circuits) for _ in range(self.tourn_size)]
-            scores = [circuit.fitness_values[0] for circuit in contestors]
+            scores = [circuit.fitness_values[self.objective_i]
+                      for circuit in contestors]
             min_score = min(scores)
             min_idx = scores.index(min_score)
 
